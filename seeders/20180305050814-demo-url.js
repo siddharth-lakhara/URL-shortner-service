81a9e9961c87719ca7dba30fbcfe2751a142e1a2
lakhara <siddharth.lakhara@gmail.com>
2018-03-06 00:20:37 +0530

    modified code for seeders

--- seeders/20180305050814-demo-url.js
+++ seeders/20180305050814-demo-url.js
@@ -1,7 +1,40 @@
-const crypto = require('crypto');
-const seedFn = require('../src/getURL');
-const helper = require('../src/helpers/urlSeeder');
-// console.log(seedArray);
+const urlShortner = require('../src/helpers/urlShortner');
+
+const changeObjectToArray = (shortURLObject) => {
+  const ArrayToReturn = Object.keys(shortURLObject).map((index) => {
+    const longurl = shortURLObject[index];
+    const shorturl = index;
+    return ({
+      longurl,
+      shorturl,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+  });
+  return ArrayToReturn;
+};
+
+const seedFn = () => {
+  const shortURLObject = {};
+  for (let i = 0; i < 100000; i += 1) {
+    let start = 0;
+    let end = 6;
+    let x = true;
+    while (x) {
+      const longurl = `http://mypersonalurl${i}.com`;
+      const shortURL = urlShortner(longurl, start, end);
+      if (shortURLObject[shortURL] === undefined) {
+        shortURLObject[shortURL] = longurl;
+        x = false;
+      } else {
+        start += 6;
+        end += 6;
+      }
+    }
+  }
+  const ArrayToInsert = changeObjectToArray(shortURLObject);
+  return ArrayToInsert;
+};
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
