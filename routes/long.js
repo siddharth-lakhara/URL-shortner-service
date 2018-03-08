3977e8498e18dca0a8f1e302e8882498e62597cb
lakhara <siddharth.lakhara@gmail.com>
2018-03-08 14:20:00 +0530

    added redis for caching

--- routes/long.js
+++ routes/long.js
@@ -1,17 +1,30 @@
 const Models = require('../models');
+const redis = require('redis');
+
+const client = redis.createClient();
 
 module.exports = [{
   method: 'GET',
   path: '/long',
   handler: (req, reply) => {
     const shorturl = req.query.shortUrl;
-    Models.urldb.findOne({ where: { shorturl } })
-      .then((searchResult) => {
-        if (searchResult === null) {
-          reply('Invalid URL');
-        } else {
-          reply(searchResult.longurl);
-        }
-      });
+    client.get(shorturl, (err, data) => {
+      if (err) {
+        console.log('Error: ', err);
+      }
+      if (data) {
+        reply(data);
+      } else {
+        Models.urldb.findOne({ where: { shorturl } })
+          .then((searchResult) => {
+            if (searchResult === null) {
+              reply('Invalid URL');
+            } else {
+              // client.set(shorturl, searchResult.longurl, redis.print);
+              reply(searchResult.longurl);
+            }
+          });
+      }
+    });
   },
 }];
