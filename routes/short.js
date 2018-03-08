e045ab2f25541febb0855d6a2b8d6f723b693634
lakhara <siddharth.lakhara@gmail.com>
2018-03-08 09:20:16 +0530

    added joi validation for url

--- routes/short.js
+++ routes/short.js
@@ -1,8 +1,6 @@
 const Joi = require('joi');
-const Models = require('../models');
 const getShortUrl = require('../src/helpers/getShortUrl');
 
-// ^ (https ?| ftp | file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]
 module.exports = [{
   method: 'POST',
   path: '/short',
@@ -10,7 +8,6 @@ module.exports = [{
     validate: {
       payload: Joi.object({
         url: Joi.string().regex(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/|www\.)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/),
-        // url: Joi.string().regex(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/)
       }),
     },
   },
