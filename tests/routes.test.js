3edceb517768c5dd036e11b81c1b1f485f3c4684
lakhara <siddharth.lakhara@gmail.com>
2018-03-08 14:29:58 +0530

    added tests for /long

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -15,3 +15,23 @@ describe('Test for shortner api', () => {
     });
   });
 });
+
+describe('Test for /long', () => {
+  test('/long gives 200 status code', (done) => {
+    server.inject('/long?shortUrl=100400', (res) => {
+      expect(res.statusCode).toBe(200);
+      done();
+    });
+  });
+
+  test('/long gives desired long url', (done) => {
+    server.inject('/long?shortUrl=100400', (res) => {
+      expect(res.payload).toEqual('http://mypersonalurl603060.com');
+      done();
+    });
+  });
+});
+
+// describe('Test for redis', ()=>{
+
+// })
